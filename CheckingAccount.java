9e1b9657f8ac6e9bb1088d7d0236e4437bc5b371
Adam Muglia <mugliaa@mail.gvsu.edu>
2015-11-03 15:43:05 -0500

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -5,10 +5,13 @@ import java.util.GregorianCalendar;
 /**********************************************************************
  * Stores information required for a checking account.
  * @author Adam Muglia & Tyler Miller
- * @version 10-28-2015
+ * @version 11-04-2015
  *********************************************************************/
 public class CheckingAccount extends Account {
+	/** Required to implement Serializable */
 	private static final long serialVersionUID = 1L;
+	
+	/** The monthly fee associated with a checking account */
 	private double monthlyFee;
 	
 	/******************************************************************
@@ -39,11 +42,18 @@ public class CheckingAccount extends Account {
 	/******************************************************************
 	 * Sets the monthly fee for the checking account.
 	 * @param monthlyFee the monthly fee of the checking account
+	 * @return void
 	 *****************************************************************/
 	public void setMonthlyFee(double monthlyFee) {
 		this.monthlyFee = monthlyFee;
 	}
 	
+	/******************************************************************
+	 * Checks if two checking accounts are equal via the number 
+	 * associated.
+	 * @param acct the account that is being compared
+	 * @return true if equal; false if not equal
+	 *****************************************************************/
 	public boolean equals(Object acct) {
 		if (acct instanceof CheckingAccount) {
 			CheckingAccount temp = (CheckingAccount) acct;
@@ -55,6 +65,10 @@ public class CheckingAccount extends Account {
 		throw new IllegalArgumentException();
 	}
 	
+	/******************************************************************
+	 * Converts a checking account into a String that lists all info.
+	 * @return the String representation of a checking account
+	 *****************************************************************/
 	public String toString() {
 		return super.toString() + getMonthlyFee();
 	}
