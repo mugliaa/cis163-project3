e6904dc6e78a139961a650e16217cec4cf336f02
Adam Muglia <mugliaa@mail.gvsu.edu>
2015-11-03 21:21:55 -0500

    Update BankModel.java

--- BankModel.java
+++ BankModel.java
@@ -55,8 +55,103 @@ public class BankModel extends AbstractTableModel implements Comparable {
 		fireTableDataChanged();
 	}
 
-	public void update(Account acct) {
+	public void update(int index) {
+		Account temp = accts.get(index);
+		if (temp instanceof CheckingAccount) {
+			CheckingAccountDialogBox c = new CheckingAccountDialogBox();
+			c.numberField.setText(Integer.toString(temp.getNumber()));
+			c.ownerField.setText(temp.getOwner());
+			c.dateOpenedField.setText(dateToString(
+					temp.getDateOpened()));
+			c.balanceField.setText(Double.toString(temp.getBalance()));
+			c.monthlyFeeField.setText(Double.toString(((
+					CheckingAccount)temp).getMonthlyFee()));
+			
+			int temp1 = c.showDialog();
+			if (temp1 == JOptionPane.OK_OPTION) {
+				String tempNum = c.numberField.getText();
+				int num = Integer.parseInt(tempNum);
+				String owner = c.ownerField.getText();
+				String tempDateOpened = 
+						c.dateOpenedField.getText();
+				String[] s = tempDateOpened.split("/");
+				int s0 = Integer.parseInt(s[0]);
+				int s1 = Integer.parseInt(s[1]);
+				int s2 = Integer.parseInt(s[2]);
+				GregorianCalendar cal = 
+						new GregorianCalendar(s2, s0, s1);
+				String tempBal = c.balanceField.getText();
+				double balance = Double.parseDouble(tempBal);
+				String tempMonthlyFee = 
+						c.monthlyFeeField.getText();
+				double monthlyFee = Double.parseDouble(
+						tempMonthlyFee);
+				
+				accts.get(index).setNumber(num);
+				accts.get(index).setOwner(owner);
+				accts.get(index).setDateOpened(cal);
+				accts.get(index).setBalance(balance);
+				((CheckingAccount)accts.get(index)).setMonthlyFee(
+						monthlyFee);
+				
+				c.reset();
+			}	
+		}
+		else {
+			SavingsAccountDialogBox s = new SavingsAccountDialogBox();
+			s.numberField.setText(Integer.toString(temp.getNumber()));
+			s.ownerField.setText(temp.getOwner());
+			s.dateOpenedField.setText(dateToString(
+					temp.getDateOpened()));
+			s.balanceField.setText(Double.toString(temp.getBalance()));
+			s.minimumBalanceField.setText(Double.toString(((
+					SavingsAccount)temp).getMinBalance()));
+			s.interestRateField.setText(Double.toString(((
+					SavingsAccount)temp).getInterestRate()));
+			
+			int t = s.showDialog();
+			
+			if (t == JOptionPane.OK_OPTION) {
+				String tempNum = s.numberField.getText();
+				int num = Integer.parseInt(tempNum);
+				String owner = s.ownerField.getText();
+				String tempDateOpened = 
+						s.dateOpenedField.getText();
+				String[] e = tempDateOpened.split("/");
+				int s0 = Integer.parseInt(e[0]);
+				int s1 = Integer.parseInt(e[1]);
+				int s2 = Integer.parseInt(e[2]);
+				GregorianCalendar c = 
+						new GregorianCalendar(s2, s0, s1);
+				String tempBal = s.balanceField.getText();
+				double balance = Double.parseDouble(tempBal);
+				String tempMin = 
+						s.minimumBalanceField.getText();
+				double minimumBal = Double.parseDouble(tempMin);
+				String tempInterest = 
+						s.interestRateField.getText();
+				double intRate = Double.parseDouble(tempInterest);
+				
+				accts.get(index).setNumber(num);
+				accts.get(index).setOwner(owner);
+				accts.get(index).setDateOpened(c);
+				accts.get(index).setBalance(balance);
+				((SavingsAccount)accts.get(index)).setMinBalance(
+						minimumBal);
+				((SavingsAccount)accts.get(index)).setInterestRate(
+						intRate);
+				
+				s.reset();
+			}
+		}
 		fireTableDataChanged();
+	}
+	
+	private String dateToString(GregorianCalendar g) {
+		int day = g.get(Calendar.DAY_OF_MONTH);
+		int month = g.get(Calendar.MONTH);
+		int year = g.get(Calendar.YEAR);
+		return "" + month + "/" + day + "/" + year;
 	}
 
 	public void clear() {
